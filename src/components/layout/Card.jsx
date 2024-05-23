a4cc925a1637486d2a91d34ddf738aca05aab156
hendricksonweib <hendricksonrocha22@gmail.com>
2024-05-23 10:32:19 -0300

    feat:[wip] card
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,4 +1,19 @@
 import React from "react";
+import Buness from '../../../public/Buness.png';
+import Idea from '../../../public/Idea.png';
+import Hat from '../../../public/Hat.png';
+import sun from '../../../public/sun.png';
+import Present from '../../../public/Present.png';
+import Rocket from '../../../public/Rocket.png';
+
+const images = {
+  Buness,
+  Idea,
+  Hat,
+  sun,
+  Present,
+  Rocket
+};
 
 const Card = ({ id, name, flipped, matched, clicked }) => {
   return (
@@ -10,7 +25,7 @@ const Card = ({ id, name, flipped, matched, clicked }) => {
     >
       <div className="back">?</div>
       <div className="front">
-        <img alt={name} src={`../../public/${name}.png`} />
+        <img alt={name} src={images[name]} />
       </div>
     </div>
   );
